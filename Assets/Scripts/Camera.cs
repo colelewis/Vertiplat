bc490be12784c003827d24d5004ecd5ff89c97e4
Kora <koraloudermilk@gmail.com>
2022-09-18 11:56:07 -0400

    add camera script

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject Player; //player object
+    public float RiseSpeed = 1.0f; //speed at which the camera will go up automatically
+    public float CatchUpLeeway = 1.0f; //how far up relative to the camera the player is allowed to get before the it catches up to them
+    public int ZAxis = -10; //sets z axis of camera
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.transform.position.y > transform.position.y+CatchUpLeeway)
+        {
+            transform.position = new Vector3(transform.position.x, Player.transform.position.y-CatchUpLeeway, ZAxis);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y+RiseSpeed*Time.deltaTime, ZAxis);
+        }
+    }
+}

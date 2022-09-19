bc345a03f2f023bdcf5c462c2378d34611aa1386
Cole Lewis <cole.g.lewis@gmail.com>
2022-09-19 13:12:09 -0400

    Further tuned spawner, much work to go. Platforms despawn automatically.

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    public float frequency = 4f;
-    public float yPlayerDeviation = 2f;
-    public float randomXDeviation;
-    public float randomYDeviation;
+    public int frequency;
+    public double yPlayerDeviation;
+    public float randomXDeviationUpperBound;
+    public float randomXDeviationLowerBound;
     public float randomYDeviationUpperBound;
     public float randomYDeviationLowerBound;
     public float platformSpawnYThreshold = 0.5f;
@@ -20,22 +20,50 @@ public class PlatformSpawner : MonoBehaviour
     void Start()
     {
         Random.InitState(randomSeed);
+        StartCoroutine(spawnPlatforms());
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 playerInCameraPosition = UnityEngine.Camera.main.WorldToViewportPoint(player.transform.position);
-        Vector3 location = Vector3.zero;
-        randomXDeviation = Random.Range(-10, 10); // to be tuned
-        randomYDeviation = Random.Range(randomYDeviationLowerBound, randomYDeviationUpperBound); // to be tuned
+        // Vector3 location = Vector3.zero;
+        // float randomXDeviation = Random.Range(randomXDeviationUpperBound, randomXDeviationLowerBound); // to be tuned
+        // float randomYDeviation = Random.Range(randomYDeviationLowerBound, randomYDeviationUpperBound); // to be tuned
 
-        location.x = playerInCameraPosition.x + randomXDeviation;
-        location.y = playerInCameraPosition.y + yPlayerDeviation + randomYDeviation;
+        // location.x = playerInCameraPosition.x + randomXDeviation;
+        // location.y = playerInCameraPosition.y + yPlayerDeviation + randomYDeviation;
 
-        if (playerInCameraPosition.y > platformSpawnYThreshold) { // spawn threshold 
-            GameObject spawnedPlatform = Instantiate(platform, location, Quaternion.identity);
+        // if (playerInCameraPosition.y > platformSpawnYThreshold) { // spawn threshold 
+        //     for (int i = 0; i < frequency; i++) {
+        //         GameObject spawnedPlatform = Instantiate(platform, location, Quaternion.identity);
+        //     }
+        // }
+        GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+        foreach (GameObject p in platforms) {
+            Vector3 platformInCameraPosition = UnityEngine.Camera.main.WorldToViewportPoint(p.transform.position);
+
+            if (platformInCameraPosition.y < 0f) { // destroy platform if it leaves camera
+                Destroy(p);
+            }
         }
         Debug.Log(playerInCameraPosition);
+    }
+
+    IEnumerator spawnPlatforms() {
+        while(true) {
+            Vector3 playerInCameraPosition = UnityEngine.Camera.main.WorldToViewportPoint(player.transform.position);
+            Vector3 location = Vector3.zero;
+            float randomXDeviation = Random.Range(randomXDeviationUpperBound, randomXDeviationLowerBound); // to be tuned
+            float randomYDeviation = Random.Range(randomYDeviationLowerBound, randomYDeviationUpperBound); // to be tuned
+
+            location.x = UnityEngine.Camera.main.transform.position.x + playerInCameraPosition.x + randomXDeviation;
+            location.y = UnityEngine.Camera.main.transform.position.y + playerInCameraPosition.y + randomYDeviation;
+            if (playerInCameraPosition.y >= platformSpawnYThreshold) { // spawn threshold 
+                GameObject spawnedPlatform = Instantiate(platform, location, Quaternion.identity);
+            }
+            yield return new WaitForSeconds(1 - playerInCameraPosition.y);
+        }
+        
     }
 }

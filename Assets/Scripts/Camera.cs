3c3704d4d72d27e98cd3e8d9398777db82eb901e
Kora Loudermilk <koraloudermilk@gmail.com>
2022-09-30 19:05:19 -0400

    increase camera rise over time

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -9,7 +9,10 @@ public class Camera : MonoBehaviour
     public float RiseSpeed = 1.0f; //speed at which the camera will go up automatically
     public float CatchUpLeeway = 1.0f; //how far up relative to the camera the player is allowed to get before it catches up to them
     public int ZAxis = -10; //sets z axis of camera
+    public float RiseSpeedIncreaseTime = 10f; //Rate in seconds that the RiseSpeed increases
+    public float RiseSpeedIncreaseRate = 0.1f; //how fast the RiseSpeed increases.
 
+    private float RiseSpeedIncreaseClock;
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +23,12 @@ public class Camera : MonoBehaviour
         else
         {
             transform.position = new Vector3(transform.position.x, transform.position.y+RiseSpeed*Time.deltaTime, ZAxis);
+        }
+        RiseSpeedIncreaseClock += Time.deltaTime;
+        if(RiseSpeedIncreaseClock>=RiseSpeedIncreaseTime)
+        {
+            RiseSpeed += RiseSpeedIncreaseRate;
+            RiseSpeedIncreaseClock = 0;
         }
     }
 }

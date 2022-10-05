11ef9749bf9fd0a8b72d17b7820d1b89cd500578
Kora Loudermilk <koraloudermilk@gmail.com>
2022-10-05 15:26:19 -0400

    fix collision script

--- Assets/Scripts/PlatformColliderToggle.cs
+++ Assets/Scripts/PlatformColliderToggle.cs
@@ -8,69 +8,45 @@ public class PlatformColliderToggle : MonoBehaviour
 
     private Collider2D PlatformCollider;
     private Collider2D PlayerCollider;
-    private float platformXSize;
-    private float platformYSize;
-    [SerializeField] private float platformRight;
-    [SerializeField] private float platformLeft;
-    [SerializeField] private float platformTop;
-    [SerializeField] private float platformBottom;
-
+    private float colliderXSize;
+    private float colliderYSize;
     private float playerXSize;
     private float playerYSize;
-    private float playerRight;
-    private float playerLeft;
-    private float playerBottom;
-    private float playerTop;
-    // This state boolean will be used to short circuit the update conditional.
-    [SerializeField] private bool solid = false;
+    // Start is called before the first frame update
     void Start()
     {
         PlatformCollider = GetComponent<Collider2D>();
-        platformXSize = PlatformCollider.bounds.size.x / 2;
-        platformYSize = PlatformCollider.bounds.size.y / 2;
-        platformRight = transform.position.x + platformXSize;
-        platformLeft = transform.position.x - platformXSize;
-        platformTop = transform.position.y + platformYSize;
-        platformBottom = transform.position.y - platformYSize;
-        // Unchanging player values.
+        colliderXSize = PlatformCollider.bounds.size.x / 2;
+        colliderYSize = PlatformCollider.bounds.size.y / 2;
+
         PlayerCollider = player.GetComponent<Collider2D>();
         playerXSize = PlayerCollider.bounds.size.x;
         playerYSize = PlayerCollider.bounds.size.y;
     }
+
+    // Update is called once per frame
     void Update()
     {
-        // Changing player values.
-        playerRight = player.transform.position.x + playerXSize / 2;
-        playerLeft = player.transform.position.x - playerXSize / 2;
-        playerTop = player.transform.position.y + playerYSize / 2;
-        playerBottom = player.transform.position.y - playerYSize / 2;
-        if (solid == true) // Collide
+        colliderXSize = PlatformCollider.bounds.size.x / 2;
+        colliderYSize = PlatformCollider.bounds.size.y / 2;
+        playerXSize = PlayerCollider.bounds.size.x;
+        playerYSize = PlayerCollider.bounds.size.y;
+        float playerRight = player.transform.position.x + playerXSize / 2 + PlayerCollider.offset.x;
+        float playerLeft = player.transform.position.x - playerXSize / 2 + PlayerCollider.offset.x;
+        float playerBottom = player.transform.position.y - playerYSize / 2 + PlayerCollider.offset.y;
+        if (playerLeft < transform.position.x + colliderXSize && playerRight > transform.position.x - colliderXSize &&
+            playerBottom < transform.position.y + colliderYSize)
         {
-            // Solid platforms are colored white.
-            GetComponentInChildren<SpriteRenderer>().color = Color.white;
-        } else if (solid == false) // Passthrough
-        {
-            // Pass-able platforms are colored red.
-            GetComponentInChildren<SpriteRenderer>().color = Color.red;
-            // Turn off colliders.
             Physics2D.IgnoreCollision(PlatformCollider, PlayerCollider, true);
-            // Solidify the platform.
-            if ( (playerBottom > platformTop) // The player's bottom MUST BE above the platform
-                  && (  (platformLeft < playerRight && playerRight < platformRight)     // The player's right side clipped within the platform or
-                      ||(platformLeft < playerLeft  && playerLeft  < platformRight) ) ) // The player's left side clipped within the platform
-            {
-                // Turn on colliders.
-                Physics2D.IgnoreCollision(PlatformCollider, PlayerCollider, false);
-                // Set State.
-                solid = true;
-            }
+            //GetComponentInChildren<SpriteRenderer>().color = Color.red;
+
         }
-        // Lines for testing calculated position variables. Because Adam's insanity.
-        
-        Debug.DrawLine(new Vector2(platformRight, platformTop), new Vector2(platformRight, platformBottom), Color.green, 0);
-        Debug.DrawLine(new Vector2(platformLeft, platformTop), new Vector2(platformLeft, platformBottom), Color.green, 0);
-        Debug.DrawLine(new Vector2(playerRight, playerTop), new Vector2(playerRight, playerBottom), Color.green, 0);
-        Debug.DrawLine(new Vector2(playerLeft, playerTop), new Vector2(playerLeft, playerBottom), Color.green, 0);
-        
+        else
+        {
+            Physics2D.IgnoreCollision(PlatformCollider, PlayerCollider, false);
+            //GetComponentInChildren<SpriteRenderer>().color = Color.white;
+        }
+        //Debug.DrawLine(new Vector3(transform.position.x + colliderXSize, transform.position.y, -3), new Vector3(transform.position.x + colliderXSize, -2, 0), Color.red, -3);
+        //Debug.DrawLine(new Vector3(transform.position.x - colliderXSize, transform.position.y, -3), new Vector3(transform.position.x - colliderXSize, -2, 0), Color.red, -3);
     }
 }
